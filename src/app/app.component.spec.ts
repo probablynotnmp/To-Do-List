68640419f9ef88ab14d6c147e27a0ce8022c668a
Prathyush N M <85440373+probablynotnmp@users.noreply.github.com>
2024-10-20 17:43:44 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('TodoList');
   });
 
-  it('should render title', () => {
+  it('should render title in h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TodoList app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('TodoList');
   });
 });
